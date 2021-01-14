9ffe6a60d6751c04199ac8c605ea61976872c41b
Quentin Guidée <quentin.guidee@gmail.com>
2021-01-14 19:07:10 +0100

    [all] Redesign

--- src/components/articles/Header.js
+++ src/components/articles/Header.js
@@ -1,9 +1,17 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 import "./sass/Header.sass";
 
 export default class Header extends Component {
     render() {
-        return <header className="header">{this.props.children}</header>;
+        return (
+            <header className="header">
+                <Link to="/" className="header__logo">
+                    Quentin Guidée
+                </Link>
+                {this.props.children}
+            </header>
+        );
     }
 }
