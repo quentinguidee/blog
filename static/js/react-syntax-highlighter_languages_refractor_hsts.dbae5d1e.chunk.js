d63fe4991b43d274f853dbec9ffda6d989eeb36f
Quentin Guidée <quentin.guidee@gmail.com>
2021-05-18 19:50:30 +0200

    Updates

---
+++ static/js/react-syntax-highlighter_languages_refractor_hsts.dbae5d1e.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpblog=this.webpackJsonpblog||[]).push([[55],{171:function(a,s,e){"use strict";function t(a){a.languages.hsts={directive:{pattern:/\b(?:max-age=|includeSubDomains|preload)/,alias:"keyword"},safe:{pattern:/\d{8,}/,alias:"selector"},unsafe:{pattern:/\d{1,7}/,alias:"function"}}}a.exports=t,t.displayName="hsts",t.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_hsts.dbae5d1e.chunk.js.map
